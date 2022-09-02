98a0ffa1887ef0f7c96e411a55f565396d80773a
Andrey Popov <andrej_1987@bk.ru>
2022-09-02 17:32:58 +0500

    Добавьте файлы проекта.

---
+++ TryCatchLab001/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TryCatchLab001
+{
+    class Authorization
+    {
+        private List<string> Names = new List<string>();
+
+        public int readNames(string _path)
+        {
+            string _buffer = "";
+            try
+            {
+                StreamReader myStreamReader = new StreamReader(_path);              
+                do
+                {
+                    _buffer = myStreamReader.ReadLine();
+                    Names.Add(_buffer);
+                    if (Names == null)
+                    {
+                        return 0;
+                    }
+                    Console.WriteLine(_buffer);
+                } while (_buffer != null);
+            }
+            catch (Exception ex001)
+            {
+
+                Console.WriteLine(ex001.Message);
+            }
+            return Names.Count;
+        }
+        public void FindeName(string name)
+        {
+            if (Names.Contains(name))
+            {
+                Console.WriteLine("Имя {0} ЕСТЬ в списке.", name);
+            }
+            else
+            {
+                Console.WriteLine("Имя {0} ОТСУТСТВУЕТ в списке.", name);
+            }
+        }
+
+    }
+}
---
+++ TryCatchLab001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchLab001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Authorization myAuthorization = new Authorization();
+            string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            _path += "\\Dwarfs.txt";
+            int count = myAuthorization.readNames(_path); 
+            count -= 1;
+            Console.WriteLine("В файле {0} {1} строк", _path, count);
+            myAuthorization.FindeName(args[0]);
+
+        }
+    }
+}
